e540345b9f0ff03dac0f8946bf40ae02ba6ed3af
guyang <sagay@vip.qq.com>
2018-08-15 18:38:08 +0800

    部署测试成功

--- src/main/java/com/ssbc/nmg/dataspider/AataspiderApplication.java
+++ src/main/java/com/ssbc/nmg/dataspider/AataspiderApplication.java
@@ -2,14 +2,23 @@ package com.ssbc.nmg.dataspider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
         "com.ssbc.nmg.dataspider","com.ssbc.nmg.dataspider.dao.mapper","com.ssbc.nmg.dataspider.dao","com.ssbc.nmg.dataspider.service"})
-public class AataspiderApplication {
+public class AataspiderApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AataspiderApplication.class, args);
+//    public static void main(String[] args) {
+//        SpringApplication.run(AataspiderApplication.class, args);
+//    }
+
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AataspiderApplication.class);
     }
 }
