a81cf6be18930575cc53d1a21ba386f7974fbbc9
kkgy333 <sagay@vip.qq.com>
2018-08-24 11:17:46 +0800

    添加JWT登录认证

--- src/main/java/com/ssbc/nmg/dataspider/Constant.java
+++ src/main/java/com/ssbc/nmg/dataspider/Constant.java
@@ -9,6 +9,6 @@ public class Constant {
      * 加密密文
      */
     public static final String JWT_SECRET = "ssbcnmghuanjingbaohuting";
-    public static final int JWT_TTL = 60*60*1000;  //millisecond
+    public static final int JWT_TTL = 5*60*60*1000;  //millisecond
 
 }
